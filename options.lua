2ceccbcacbd6d72eced51b4e5eca675751261dec
alexmtc765 <74274602+alexmtc765@users.noreply.github.com>
2021-09-07 20:48:03 -0400

    Made options better

--- options.lua
+++ options.lua
@@ -1,45 +1,26 @@
---get saved fov
 function init()
     defaultFOV = GetInt("savegame.mod.defaultFOV")
     zoomedFOV = GetInt("savegame.mod.zoomedFOV")
     zoomSpeed = GetFloat("savegame.mod.zoomSpeed")
 end
 
---draws ui
 function draw()
     centerUI()
     UiFont("bold.ttf", 48)
-    
-    UiTranslate(0, 550)
-    defaultFOV = UiSlider("ui/common/dot.png", "x", defaultFOV, 0, 170)
-    defaultFOV = round(defaultFOV)
-    UiTranslate(-100, 0)
-    UiText(defaultFOV,true)
+    drawBottomButtons()
+    UiTranslate(0,-100)
+    UiTranslate(0,300)
+    drawSliders()
+    UiTranslate(0,-300)
+    UiTranslate(0,345)
+    drawZoomSpeed()
+    UiTranslate(0,-345)
+    drawText()
+end
 
-    UiTranslate(100, 0)
-    zoomedFOV = UiSlider("ui/common/dot.png", "x", zoomedFOV, 0, 170)
-    zoomedFOV = round(zoomedFOV) 
-    UiTranslate(-100, 0)
-    UiText(zoomedFOV,true)
-    
-    zoomSpeed = roundDec(zoomSpeed)
-    UiTranslate(100, 0)
-    UiTranslate(-100, 0)
-    if UiTextButton("+0.1") then
-        zoomSpeed = zoomSpeed + 0.1
-    end
-    UiTranslate(100, 0)
-    if UiTextButton("-0.1") then
-        zoomSpeed = zoomSpeed - 0.1 
-        if (zoomSpeed < 0) then
-            zoomSpeed = 0
-        end
-    end 
-    UiTranslate(100, 0)
-    UiText(zoomSpeed,true)
-    UiTranslate(-100, 0)
-    UiTranslate(0, -100) 
-    UiTranslate(0, 150)
+--draws menu save and default button
+function drawBottomButtons()
+    UiTranslate(0,750)
     if UiTextButton("Menu") then
         Menu()
     end
@@ -60,7 +41,56 @@ function draw()
         SetInt("savegame.mod.zoomedFOV", zoomedFOV)
         SetFloat("savegame.mod.zoomSpeed", zoomSpeed)
     end
-    --debugUI()
+    UiTranslate(800, -750)   
+end
+
+function drawSliders()
+    
+    defaultFOV = UiSlider("ui/common/dot.png", "x", defaultFOV, 0, 170)
+    defaultFOV = round(defaultFOV)
+    UiTranslate(-100, 0)
+    UiText(defaultFOV,true)
+
+    UiTranslate(0, 50)
+
+    UiTranslate(100, 0)
+    zoomedFOV = UiSlider("ui/common/dot.png", "x", zoomedFOV, 0, 170)
+    zoomedFOV = round(zoomedFOV) 
+    UiTranslate(-100, 0)
+    UiText(zoomedFOV,true)
+end
+
+function drawZoomSpeed()
+    zoomSpeed = roundDec(zoomSpeed)
+    if UiTextButton("+0.1") then
+        zoomSpeed = zoomSpeed + 0.1
+    end
+    UiTranslate(100, 0)
+    if UiTextButton("-0.1") then
+        zoomSpeed = zoomSpeed - 0.1 
+        if (zoomSpeed < 0) then
+            zoomSpeed = 0
+        end
+    end
+    UiTranslate(100, 0)
+    UiText(zoomSpeed,true)
+    UiTranslate(-100, 0)
+    UiTranslate(0, -100) 
+    UiTranslate(0, 150)
+end
+
+function drawText()
+    UiText("Default FOV")
+    UiTranslate(0,100)
+    UiText("Zoomed FOV")
+    UiTranslate(0,100)
+    UiText("Zoom Speed")
+    UiTranslate(0,-200)
+end
+
+function centerUI()
+    UiTranslate(UiCenter(), 250)
+	UiAlign("center middle")
 end
 
 function round(n)
@@ -68,25 +98,6 @@ function round(n)
 end
 
 function roundDec(n)
-    	 return math.floor(n * 100 + 0.5) / 100
-    	end
-    
-
-function centerUI()
-    UiTranslate(UiCenter(), 250)
-	UiAlign("center middle")
+    return math.floor(n * 100 + 0.5) / 100
 end
-
-function debugUI()
-    UiTranslate(0, -100)
-    local dfov = GetInt("savegame.mod.defaultFOV")
-    UiText(dfov,true)
     
-    UiTranslate(0, -100)
-    local zfov = GetInt("savegame.mod.zoomedFOV")
-    UiText(zfov,true)
-    
-    UiTranslate(0, -100)
-    local zSpeed = GetFloat("savegame.mod.zoomSpeed")
-    UiText(zSpeed,true)
-end
